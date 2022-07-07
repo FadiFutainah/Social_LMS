29fdfe334f513ed8dc8c32416547e651381980e6
FadiFutainah <fadiftenaft@gmail.com>
2022-07-07 12:38:05 +0300

    edit cv models based on the ERD

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,4 @@
+from MySQLdb import Date
 from django.db import models
 from  django.contrib.auth.models import User
 
@@ -14,20 +15,24 @@ class Profile(models.Model):
     name = models.CharField(max_length=255)
     gender = models.CharField(choices=genders, max_length=1)
     points = models.PositiveBigIntegerField()
-    study_year = models.CharField(max_length=4)
     age = models.PositiveIntegerField()
     photo = models.ImageField(null=True)
     address = models.CharField(max_length=255)
     services = models.TextField()
     bio = models.TextField()
+    birth_date = models.DateField(null=True)
+    is_graduated = models.BooleanField(default=False)
+    start_date = models.DateField(null=True)    
+    end_date = models.DateField(null=True)    
     
 class Experience(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    type = models.CharField(max_length=55)
     description = models.TextField()
     start_date = models.DateField()
-    end_date = models.DateField()
-    type = models.CharField(max_length=55)
+    end_date = models.DateField(null=True)
+    is_certified = models.BooleanField(default=False)
     
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -36,12 +41,20 @@ class Contact(models.Model):
     
 class Project(models.Model):
     title = models.CharField(max_length=255)
-    end_date = models.DateField()
+    end_date = models.DateField(null=True)
     start_date = models.DateField()
     description = models.TextField()
-    projects = models.ManyToManyField(Profile, through='Membership')
+    link = models.URLField(null=True)
+    members = models.ManyToManyField(Profile, through='Membership', verbose_name='members')
+    is_cerified = models.BooleanField(default=False)
     
 class Membership(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     position = models.CharField(max_length=255)
+    
+class Mark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject_name = models.CharField(max_length=255)
+    mark = models.PositiveIntegerField()
+    data = models.DateField()
