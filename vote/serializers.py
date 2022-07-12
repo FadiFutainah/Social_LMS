5492eff6153a54b1f89f15941f3162e3ff84880f
suliman <sulimanawadstudy@gmail.com>
2022-07-12 04:18:30 +0300

    add the __all__ serializers

---
+++ vote/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from models import *
+
+class VotedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VotedItem
+        fields = '__all__'
